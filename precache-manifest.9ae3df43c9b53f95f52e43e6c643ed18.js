005eb0c5e8e104a915c9c9fd6ca8ca314d5ebcc1
Amisha <amisha@bigscal.com>
2020-10-12 00:19:35 -0700

    Updates

---
+++ precache-manifest.9ae3df43c9b53f95f52e43e6c643ed18.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f7ccf6b2623d48a214be7ddc2247b2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "acbfd526ed52cc328c02",
+    "url": "/my-app/static/css/2.7b1f77b4.chunk.css"
+  },
+  {
+    "revision": "f0903d08620f455f337d",
+    "url": "/my-app/static/css/main.767143e1.chunk.css"
+  },
+  {
+    "revision": "acbfd526ed52cc328c02",
+    "url": "/my-app/static/js/2.6ffca12a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.6ffca12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0903d08620f455f337d",
+    "url": "/my-app/static/js/main.6ca9c78f.chunk.js"
+  },
+  {
+    "revision": "f2c2e4de459ee843d4d3",
+    "url": "/my-app/static/js/runtime-main.bc73d78c.js"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/my-app/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/my-app/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "25ab413e557b6deff1d28beaf72b83dd",
+    "url": "/my-app/static/media/bg.25ab413e.png"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/my-app/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/my-app/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/my-app/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "db57da795c9c521b14cf9f487da06d05",
+    "url": "/my-app/static/media/dribble.db57da79.svg"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/my-app/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "48802972971d785901ffe84c82defd7b",
+    "url": "/my-app/static/media/hero-image.48802972.png"
+  },
+  {
+    "revision": "789bce482c497ca5006b97bfe6f0f91a",
+    "url": "/my-app/static/media/logo-yellow.789bce48.svg"
+  },
+  {
+    "revision": "166136cdfdad6ba392ae1d7650bf2da9",
+    "url": "/my-app/static/media/logo.166136cd.svg"
+  },
+  {
+    "revision": "39c5f3c6bdad4789fa38e3975946fe3f",
+    "url": "/my-app/static/media/logo.39c5f3c6.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/my-app/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/my-app/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "1f6de9f03ce7d4129313827d29a875d1",
+    "url": "/my-app/static/media/partner01.1f6de9f0.svg"
+  },
+  {
+    "revision": "d8f2a498f49a22e58484e1be708fbcf0",
+    "url": "/my-app/static/media/partner02.d8f2a498.svg"
+  },
+  {
+    "revision": "0d091731c341987d1a26f18053352817",
+    "url": "/my-app/static/media/partner03.0d091731.svg"
+  },
+  {
+    "revision": "81292e3cc0ec62e529d83a0df681f975",
+    "url": "/my-app/static/media/partner04.81292e3c.svg"
+  },
+  {
+    "revision": "d39e28a33d995a61c2338ac40eca3c67",
+    "url": "/my-app/static/media/partner05.d39e28a3.svg"
+  },
+  {
+    "revision": "a0c63ad5babbf86025666de007e4af70",
+    "url": "/my-app/static/media/partner06.a0c63ad5.svg"
+  },
+  {
+    "revision": "ed2884eab293a385c09a04a88d2be2ad",
+    "url": "/my-app/static/media/person01.ed2884ea.png"
+  },
+  {
+    "revision": "cb7c49f4630b07c01489b3dd71914816",
+    "url": "/my-app/static/media/person02.cb7c49f4.png"
+  },
+  {
+    "revision": "082a47c553a3668b1c4e6abdf5b3a3a4",
+    "url": "/my-app/static/media/preview.082a47c5.png"
+  },
+  {
+    "revision": "31ab443473049dd10cc2a2d60e8736fd",
+    "url": "/my-app/static/media/preview.31ab4434.png"
+  },
+  {
+    "revision": "33b61a36d53a39f25d40785c410e8afb",
+    "url": "/my-app/static/media/preview.33b61a36.png"
+  },
+  {
+    "revision": "84d0ccdb03382efcd450371d69900f26",
+    "url": "/my-app/static/media/preview.84d0ccdb.png"
+  },
+  {
+    "revision": "bd035d33d9abc45ecdba03afebc8b19b",
+    "url": "/my-app/static/media/preview.bd035d33.png"
+  },
+  {
+    "revision": "d014391375a7b6e59afbcb8b522d386e",
+    "url": "/my-app/static/media/preview.d0143913.png"
+  },
+  {
+    "revision": "0ecdf21ae145f462c5258daad33c2dda",
+    "url": "/my-app/static/media/twitter.0ecdf21a.svg"
+  }
+]);
